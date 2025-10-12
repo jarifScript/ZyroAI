7712b4925917a4c8d5335bb95da6fbb27d31c3cc
Jarif <jarifhasan4202@gmail.com>
2025-10-13 04:39:51 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-set -o errexit
-
-# --- ADD THESE TWO LINES TO FORCE PYTHON 3.11.8 ---
-pyenv install 3.11.8 -s
-pyenv global 3.11.8
-# --------------------------------------------------
+apt-get update && apt-get install -y \
+  ffmpeg \
+  tesseract-ocr \
+  poppler-utils
