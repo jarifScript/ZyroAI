1b8edd9be960f85b5b2f349630fafb5661144311
Jarif <jarifhasan4202@gmail.com>
2025-10-13 03:46:24 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# --- NEW LINES: Force Python 3.11.8 for compatibility ---
+# --- ADD THESE TWO LINES TO FORCE PYTHON 3.11.8 ---
 pyenv install 3.11.8 -s
 pyenv global 3.11.8
-# --------------------------------------------------------
+# --------------------------------------------------
 
 apt-get update
 apt-get install -y tesseract-ocr tesseract-ocr-ben poppler-utils ffmpeg
